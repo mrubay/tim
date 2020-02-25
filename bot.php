e989d2ba444029c3719cda1786a6eb3f9023af34
Mr.Ubay <61289643+mrubay@users.noreply.github.com>
2020-02-25 15:04:07 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -173,7 +173,7 @@ echo color("blue"," ======================================\n");
             echo color("yellow","!] Silahkan input kembali\n");
             goto otp;
             }
-         else{
+            }
          echo color("red","-] Nomor sudah teregistrasi");
          echo"\n==================================\n\n";
          echo color("yellow","!] Silahkan registrasi kembali\n");
