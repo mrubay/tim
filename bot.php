f2ae3d23d1ecec7deeab9ae01bf1afc67f835252
Mr.Ubay <61289643+mrubay@users.noreply.github.com>
2020-02-26 18:19:40 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -151,7 +151,6 @@ echo color("grey"," ======================================\n");
          echo"\n==================================\n\n";
          echo color("yellow","!] Silahkan input kembali\n");
          goto otp;
-         }
          }else{
          echo color("red","NOMOR SUDAH TERDAFTAR/SALAH !!!");
          echo "\nMau ulang? (y/n): ";
