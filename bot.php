9e493a8217341012c384996f0853879358f20b4d
Mr.Ubay <61289643+mrubay@users.noreply.github.com>
2020-03-10 19:52:41 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -61,7 +61,7 @@ echo color("red"," ======================================\n");
         echo color("yellow",".");
         sleep(1);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD090320A"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOOD022620A"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("red","+] Message: ".$message);
@@ -75,7 +75,7 @@ echo color("red"," ======================================\n");
         sleep(1);
         }
         sleep(3);
-        $boba10 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOOD022620A"}');
+        $boba10 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD090320A"}');
         $messageboba10 = fetch_value($boba10,'"message":"','"');
         if(strpos($boba10, 'Promo kamu sudah bisa dipakai.')){
         echo "\n".color("red","+] Message: ".$messageboba10);
