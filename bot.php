0e94925186253e89b5a9e95c8c73a8f59e8ba914
Mr.Ubay <61289643+mrubay@users.noreply.github.com>
2020-03-10 18:18:13 +0700

    Update Kode Voucher
    Update Voucher Gofood

--- bot.php
+++ bot.php
@@ -75,7 +75,7 @@ echo color("red"," ======================================\n");
         sleep(1);
         }
         sleep(3);
-        $boba10 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOOD022620B"}');
+        $boba10 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD090320A"}');
         $messageboba10 = fetch_value($boba10,'"message":"','"');
         if(strpos($boba10, 'Promo kamu sudah bisa dipakai.')){
         echo "\n".color("red","+] Message: ".$messageboba10);
